9a1c1d81cb2c8c8dcb99f8910b42b0a763a29d2c
tong.hai.nam <tong.hai.nam@sun-asterisk.com>
2023-07-06 14:15:26 +0700

    giai bai tap 3

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        int a, b;
+        float x;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhap a:");
+        a = input.nextInt();
+        System.out.println("Nhap b:");
+        b = input.nextInt();
+        System.out.println("Nhap x:");
+        x = input.nextFloat();
+
+        double kq = a / 10 + 
+                Math.pow(a, b) -
+                Math.sin(Math.PI * x / 3) / Math.sqrt(Math.pow(a + b, 2) + 5) +
+                Math.log10(Math.pow(a, 2) + 1) -
+                Math.log(Math.pow(4 * x, 2 * a) + 3) / 2.55;
+
+        System.out.println("ket qua:");
+
+        System.out.format("%.3f%n", kq);
+    }
+}
